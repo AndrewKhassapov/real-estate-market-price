4fd614a486afb04f72d71bb91154c8810b038a25
Andrew Khassapov <53222142+AndrewKhassapov@users.noreply.github.com>
2021-08-15 17:50:35 +1000

    Update getRealEstatePrice.js
    Added function to display the advertised and expected prices on the RealEstate.com.au listing,
    If there is no price listed.

--- getRealEstatePrice.js
+++ getRealEstatePrice.js
@@ -8,10 +8,28 @@ function getRealEstatePrice()
     {
         let adPrice = REA.targeting.ad_price;
         let listPrice = REA.targeting.listing_price;
-        return ("Advertised price: $" + adPrice + "\n\nExpected listing price: $" + listPrice);
+        return ("Advertised price: $" + adPrice.replace("_","-") + "\n\nExpected listing price: $" + listPrice.replace("_","-"));
     }
     return "Realestate.com.au has been updated and this is no longer valid. To be continued!";
 }
 
+/*
+* Displays the algorithmic prices to the 'property-price' element
+* if no price is displayed
+* for realestate.com.au properties.
+*/
+function displayRealEstatePrice()
+{   
+    let element = document.getElementsByClassName("property-price")[0];
+    if(!element.textContent.includes("$"))
+    { 
+        // Display algorithmic price if no price is shown.
+        element.textContent = getRealEstatePrice();
+    }
+}
+
 // Print the price to the console.
 console.log(getRealEstatePrice());
+
+// Display price on web site.
+displayRealEstatePrice();
